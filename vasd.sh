161a61dcb60b15b8ad8cd3a1c997ef016a484b12
JaysonHurst <jayson_hurst@dell.com>
2015-03-23 14:53:56 -0600

    Fix Bug# 857 - Remove checks for Ubuntu SELinux

--- vasd.sh
+++ vasd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+PROJECT_NAME="vasd"
 DIRNAME=`dirname $0`
 cd $DIRNAME
 USAGE="$0 [ --update ][ clean ]"
@@ -8,75 +9,58 @@ echo 'You must be root to run this script'
 exit 1
 fi
 
-get_platform()
-{
-	case `uname -s` in
-		Linux)
-			PLATFORM=`lsb_release -a | grep Distributor | awk '{print $3}'`
-			;;
-		*)
-			echo "Platform is not supported"
-			;;
-	esac
-}
-
-set_Makefile()
-{
-    get_platform
-
-	case "$PLATFORM" in
-		Ubuntu)
-			MAKEFILE="/usr/share/selinux/default/include/Makefile"
-		;;
-		RedHatEnterpriseServer)
-			MAKEFILE="/usr/share/selinux/devel/Makefile"
-		;;
-		*)
-	        	MAKEFILE="/usr/share/selinux/devel/Makefile"
-        	;;
-	esac
-}
-
-set_Makefile
+MAKEFILE="/usr/share/selinux/devel/Makefile"
 
 if [ $# -eq 1 ]; then
-	if [ "$1" = "--update" ] ; then
-		time=`ls -l --time-style="+%x %X" vasd.te | awk '{ printf "%s %s", $6, $7 }'`
-		rules=`ausearch --start $time -m avc --raw -se vasd`
-		if [ x"$rules" != "x" ] ; then
-			echo "Found avc's to update policy with"
-			echo -e "$rules" | audit2allow -R
-			echo "Do you want these changes added to policy [y/n]?"
-			read ANS
-			if [ "$ANS" = "y" -o "$ANS" = "Y" ] ; then
-				echo "Updating policy"
-				echo -e "$rules" | audit2allow -R >> vasd.te
-				# Fall though and rebuild policy
-			else
-				exit 0
-			fi
-		else
-			echo "No new avcs found"
-			exit 0
-		fi
+        if [ "$1" = "--update" ] ; then
+                time=`ls -l --time-style="+%x %X" $PROJECT_NAME.te | awk '{ printf "%s %s", $6, $7 }'`
+                rules=`ausearch --start $time -m avc --raw -se $PROJECT_NAME`
+                if [ x"$rules" != "x" ] ; then
+                        echo "Found avc's to update policy with"
+                        echo -e "$rules" | audit2allow -R
+                        echo "Do you want these changes added to policy [y/n]?"
+                        read ANS
+                        if [ "$ANS" = "y" -o "$ANS" = "Y" ] ; then
+                                echo "Updating policy"
+                                echo -e "$rules" | audit2allow -R >> $PROJECT_NAME.te
+                                # Fall though and rebuild policy
+                        else
+                                exit 0
+                        fi
+                else
+                        echo "No new avcs found"
+                        exit 0
+                fi
     elif [ "$1" = "clean" ]; then
-                echo "Cleaning Policy"
-                set -x
-        	make -f "$MAKEFILE" clean || exit
-                exit 0
-	else
-		echo -e $USAGE
-		exit 1
-	fi
+        echo "Cleaning $PROJECT_NAME Policy"
+        set -x
+        make -f "$MAKEFILE" clean || exit
+        exit 0
+    elif [ "$1" = "make" ]; then
+        echo "Making $PROJECT_NAME Policy"
+        set -x
+        make -f "$MAKEFILE" || exit
+        exit 0
+    elif [ "$1" = "remove" ]; then
+        echo "Removing $PROJECT_NAME Policy"
+        set -x
+        semanage fcontext -m -t var_auth_t "/var/opt/quest/vas/vasd(/.*)?"
+        semodule -r vasd
+        exit 0
+    else
+        echo -e $USAGE
+        exit 1
+    fi
 elif [ $# -ge 2 ] ; then
-	echo -e $USAGE
-	exit 1
+    echo -e $USAGE
+    exit 1
 fi
 
 echo "Building and Loading Policy"
 set -x
 make -f "$MAKEFILE" || exit
-/usr/sbin/semodule -i vasd.pp
+
+/usr/sbin/semodule -i $PROJECT_NAME.pp
 
 # Fixing the file context on /etc/rc\.d/init\.d/vasd
 /sbin/restorecon -F -R -v /etc/rc\.d/init\.d/vasd
@@ -85,3 +69,6 @@ make -f "$MAKEFILE" || exit
 /sbin/restorecon -F -R -v /opt/quest/
 /sbin/restorecon -F -R -v /etc/opt/quest/
 /sbin/restorecon -F -R -v /var/opt/quest/
+
+# Restart the vasd daemon so it picks up the new policy.
+service vasd restart
