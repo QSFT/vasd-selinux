10c186bcd32ee14c620a7b28f8876a149ade5135
JaysonHurst <jayson.hurst@quest.com>
2018-05-14 09:31:54 -0600

    Updated vasd.sh script to be more robust and useful

--- vasd.sh
+++ vasd.sh
@@ -1,15 +1,29 @@
 #!/bin/sh -e
-## Restore path for users /home dirs
+# set -e and stop on all errors
+
+#==============================================================================
+# Copyright 2018 One Identity LLC. ALL RIGHTS RESERVED.
+#
+# Version: 4.1.5.23440
+#
+#==============================================================================
+
+# Restore path for users /home dir
 RESTORE_PATH="/home/"
 
 PROJECT_NAME="vasd"
-DIRNAME=`dirname $0`
-cd $DIRNAME
-USAGE="$0 [ --update ][ clean ]"
-if [ `id -u` != 0 ]; then
-echo 'You must be root to run this script'
-exit 1
-fi
+DIRNAME=$(dirname $0)
+
+cd "$DIRNAME"
+
+MAKEFILE="/usr/share/selinux/devel/Makefile"
+test -f $MAKEFILE || ( echo "Missing $MAKEFILE, dependencies not met"; exit 1; )
+
+SEMANAGE="$(which semanage)"
+SEMODULE="$(which semodule)"
+RESTORECON="$(which restorecon)"
+AUDIT2ALLOW="$(which audit2allow)"
+MATCHPATHCON="$(which matchpathcon)"
 
 #-- prompt user for information
 #   usage: query prompt varname [default]
@@ -38,88 +52,189 @@ yesorno () {
     done
 }
 
-MAKEFILE="/usr/share/selinux/devel/Makefile"
+check_if_root()
+{
+        if [ `id -u` != 0 ]; then
+                echo 'You must be root to run this command'
+                return 1
+        fi
+}
 
-if [ $# -eq 1 ]; then
-        if [ "$1" = "--update" ] ; then
+adjust_fcontext()
+{
+        fcontext_from="$1"
+        fcontext_to="$2"
+
+        if [ -x "$SEMANAGE" ]; then
+                $SEMANAGE fcontext -m -t $fcontext_from "$fcontext_to"
+        fi
+}
+
+restore_fcontext()
+{
+        fcontext_filename="$1"
+        $RESTORECON -F -R -v "$fcontext_filename"
+}
+
+do_usage()
+{
+        echo "$0 [ clean | make | add | remove | update | status ]"
+}
+
+do_clean()
+{
+        echo "Cleaning $PROJECT_NAME SELinux Policy"
+        make -f "$MAKEFILE" clean || return 1
+        return 0
+}
+
+do_make()
+{
+        echo "Building $PROJECT_NAME SELinux Policy"
+        make -f "$MAKEFILE" || return 1
+        return 0
+}
+
+do_add()
+{
+        # Check if we are running as root
+        check_if_root
+        # Clean the policy
+        do_clean
+        # Make the policy
+        do_make
+        echo -n "Adding $PROJECT_NAME SELinux Policy ... "
+        $SEMODULE -i $PROJECT_NAME.pp && echo "OK" || echo "FAILED"
+
+        # RHEL bug# 1141967 causes setting the fcontext of "/var/opt/quest/vas/vasd(/.*)?"
+        # in the *.fc file to fail.
+        # Always adjust the fcontext of "/var/opt/quest/vas/vasd(/.*)?" by hand in order to 
+        # work around this bug. Reset to system default on removal.
+        adjust_fcontext "vasd_var_auth_t" "/var/opt/quest/vas/vasd(/.*)?"
+
+        restore_fcontext "/etc/rc.d/init.d/vasd"
+        restore_fcontext "/etc/init.d/vasd"
+        restore_fcontext "/opt/quest"
+        restore_fcontext "/etc/opt/quest"
+        restore_fcontext "/var/opt/quest"
+
+        # Restart the vasd daemon if it is already running so it picks up the new policy.
+        service vasd status >/dev/null && service vasd restart
+
+        return 0
+}
+
+do_remove()
+{
+        # Check if we are running as root
+        check_if_root
+        echo -n "Removing $PROJECT_NAME SELinux Policy ... "
+        adjust_fcontext "var_auth_t" "/var/opt/quest/vas/vasd(/.*)?"
+        $SEMODULE -r $PROJECT_NAME && echo "OK" || echo "FAILED"
+
+        # Restart the vasd daemon if it is already running so it picks up the new policy.
+        service vasd status >/dev/null && service vasd restart
+
+        return 0
+}
+
+do_update()
+{
+        # Check if we are running as root
+        check_if_root
+        echo "Updating $PROJECT_NAME SELinux policy"
+        # Check to see if we are in an interactive shell
+        if [[ -t 0 || -p /dev/stdin ]]; then
                 time=`ls -l --time-style="+%x %X" $PROJECT_NAME.te | awk '{ printf "%s %s", $6, $7 }'`
+                set +e
                 rules=`ausearch --start $time -m avc --raw -se $PROJECT_NAME`
                 if [ x"$rules" != "x" ] ; then
+                        set -e
                         echo "Found avc's to update policy with"
-                        echo -e "$rules" | audit2allow -R
-                        echo "Do you want these changes added to policy [y/n]?"
-                        read ANS
-                        if [ "$ANS" = "y" -o "$ANS" = "Y" ] ; then
+                        echo -e "$rules" | $AUDIT2ALLOW -R
+                        if yesorno "Do you want these changes added to $PROJECT_NAME policy?"; then
                                 echo "Updating policy"
-                                echo -e "$rules" | audit2allow -R >> $PROJECT_NAME.te
-                                # Fall though and rebuild policy
-                        else
-                                exit 0
+                                echo -e "$rules" | $AUDIT2ALLOW -R >> $PROJECT_NAME.te
+                                # Rebuild policy
+                                do_add
                         fi
                 else
                         echo "No new avcs found"
-                        exit 0
+                        set -e
                 fi
-    elif [ "$1" = "clean" ]; then
-        echo "Cleaning $PROJECT_NAME Policy"
-        set -x
-        make -f "$MAKEFILE" clean || exit
-        exit 0
-    elif [ "$1" = "make" ]; then
-        echo "Making $PROJECT_NAME Policy"
-        set -x
-        make -f "$MAKEFILE" || exit
-        exit 0
-    elif [ "$1" = "remove" ]; then
-        echo "Removing $PROJECT_NAME Policy"
-        set -x
-        if [ -x "/usr/sbin/semanage" ]; then
-            semanage fcontext -m -t var_auth_t "/var/opt/quest/vas/vasd(/.*)?"
+        else
+                echo "Update is not supported in a non-interactive shell"
+                return 1
         fi
-        semodule -r vasd
-        exit 0
-    else
-        echo -e $USAGE
-        exit 1
-    fi
-elif [ $# -ge 2 ] ; then
-    echo -e $USAGE
-    exit 1
+
+        return 0
+}
+
+do_restore()
+{
+		restore_path="${1:-"$RESTORE_PATH"}"
+        # Check if we are running as root
+        check_if_root
+        if [ ! -d "$restore_path" ]; then
+            echo "Invalid file path: "$restore_path""
+            return 1
+        fi
+        echo "Restoring files(s) default SELinux security contexts for "$restore_path""
+        set +ex
+        # Check the $RESTORE_PATH (Which is /home/) to see if any of the labels are wrong
+        output=`$RESTORECON -viRn "$restore_path" | /bin/awk '{print $3}'`
+        match_output=`$MATCHPATHCON -V $output`
+
+        ## If we have output here then print out the wrong lables and ask if we should restore them.
+        if [ -n "$output" ]; then
+                printf "The following files from your "$restore_path" have the incorrect SELinux contexts\n"
+                echo "$match_output"
+                if yesorno "Do you want to restore the default SELinux security contexts on the $restore_path directory?"; then
+                        restore_fcontext "$restore_path"
+                        # Restart the vasd daemon if it is already running so it picks up the new policy.
+                        service vasd status >/dev/null && service vasd restart
+                fi
+        else
+                echo "$restore_path SELinux security contexts look correct"
+        fi
+        set -e
+
+        return 0
+}
+
+do_status()
+{
+        echo -n "Checking if $PROJECT_NAME SELinux policy is installed ... "
+        $SEMODULE -l | grep -w ^"$PROJECT_NAME" >/dev/null && ( echo "Yes"; return 0) || ( echo "No"; return 1 )
+}
+
+if [ $# -ge 1 ]; then
+        case "$1" in
+                "clean")
+                        do_clean
+                ;;
+                "make")
+                        do_make
+                ;;
+                "add")
+                        do_add
+                ;;
+                "remove")
+                        do_remove
+                ;;
+                "update")
+                        do_update
+                ;;
+                "status")
+                        do_status
+                ;;
+                "restore")
+                        do_restore "$2"
+                ;;
+                *)
+                        do_usage
+                ;;
+        esac
+else
+        do_usage
 fi
-
-echo "Building and Loading Policy"
-set -x
-make -f "$MAKEFILE" || exit
-
-/usr/sbin/semodule -i $PROJECT_NAME.pp
-
-if [ -x "/usr/sbin/semanage" ]; then
-    semanage fcontext -m -t vasd_var_auth_t "/var/opt/quest/vas/vasd(/.*)?"
-fi
-
-# Fixing the file context on /etc/rc\.d/init\.d/vasd
-/sbin/restorecon -F -R -v /etc/rc\.d/init\.d/vasd
-/sbin/restorecon -F -R -v /etc/init\.d/vasd
-
-/sbin/restorecon -F -R -v /opt/quest/
-/sbin/restorecon -F -R -v /etc/opt/quest/
-/sbin/restorecon -F -R -v /var/opt/quest/
-
-set +ex
-## Check the $RESTORE_PATH (Which is /home/) to see if any of the labels are wrong
-output=`/sbin/restorecon -viRn "$RESTORE_PATH" | /bin/awk '{print $3}'`
-match_output=`/sbin/matchpathcon -V $output`
-
-## If we have output here then print out the wrong lables and ask if we should restore them.
-if [ -n "$output" ]; then
-    printf "The following files from your "$RESTORE_PATH" have the incorrect SELinux contexts\n"
-    echo "$match_output"
-    if yesorno "Do you want to restore the default SELinux security contexts on the user's /home directory?"; then
-        /sbin/restorecon -F -R -v "$RESTORE_PATH"
-    fi
-fi
-set -e
-
-# Restart the vasd daemon if it is already running so it picks up the new policy.
-service vasd status && service vasd restart
-
